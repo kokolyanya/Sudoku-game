310d3f664916ae6ec2fdbc1624d5f3ec8de4e3ff
kokolyanya <kokolyanya@gmail.com>
2023-10-14 15:25:32 +0300

    ajout du play pour les utilisateurs

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,6 @@
-def findNextEmpty(puzzle):
+import random, re
+
+def findNextEmpty(puzzle) :
     #finds the next row, col on the puzzle that's not filled yet
     #return row, col or None, None if there is none
 
@@ -8,7 +10,7 @@ def findNextEmpty(puzzle):
                 return r, c
     return None, None
 
-def isValid(puzzle, guess, row, col):
+def isValid(puzzle, guess, row, col) :
     #check the row
     rowVals = puzzle[row]
     if guess in rowVals:
@@ -32,7 +34,7 @@ def isValid(puzzle, guess, row, col):
     return True
 
 
-def solveSudoku(puzzle):
+def solveSudoku(puzzle) :
     #step 1 : choose somewhere on the puzzle to make a guess
     row, col = findNextEmpty(puzzle)
     if row == None:
@@ -54,21 +56,83 @@ def solveSudoku(puzzle):
     #step 6 : if none of the numbers that we try work, then this puzzle is unsolvable
     return False
 
-if __name__=='__main__':
-    exampleBoard = [
-        [3, 9, -1,   -1, 5, -1,   -1, -1, -1],
-        [-1, -1, -1,  2, -1, -1,  -1, -1, 5],
-        [-1, -1, -1,  7, 1, 9,    -1, 8, -1],
-        
-        [-1, 5, -1,   -1, 6, 8,    -1, -1, -1],
-        [2, -1, 6,    -1, -1, 3,   -1, -1, -1],
-        [-1, -1, -1,  -1, -1, -1,  -1, -1, 4],
-        
-        [5, -1, -1,  -1, -1, -1,  -1, -1, -1],
-        [6, 7, -1,   -1, -1, 5,   -1, 4, -1],
-        [1, -1, 9,   -1, -1, -1,   2, -1, -1],
-    ]
-    print(solveSudoku(exampleBoard))
-    print(exampleBoard)
+def newPuzzle() :
+    puzzle = [[-1 for _ in range(9)] for _ in range(9)]
+    compteur = 0
+    row = 0    #the row
+    col = 0    #the column
+    while compteur < 9:
+        guess = random.randint(1, 9)    #the number
+        if isValid(puzzle, guess, row, col):
+            puzzle[row][col] = guess
+            col += 1
+            compteur += 1
 
+    return puzzle
+
+def userPuzzle(puzzle) :
+    userPuzzle = [[0 for _ in range(9)] for _ in range(9)]
+    compteur = 0
+    while compteur < 70:
+        row = random.randint(0, 8)    #the row
+        col = random.randint(0, 8)    #the col
+        if userPuzzle[row][col] == 0 :
+            userPuzzle[row][col] = puzzle[row][col]
+            compteur += 1
+
+    return userPuzzle
+
+def play(puzzle, userPuzzle) :
+    lives = 3
+    while lives > 0 and puzzle != userPuzzle:
+        printPuzzle(userPuzzle)
+        userInput = re.split(',(\\s)*',input("Choose a square to fill. Input as row,col : "))
+        try:
+            row, col = int(userInput[0])-1, int(userInput[-1])-1
+        except ValueError:
+            continue
+        if row<0 or row>=9 or col<0 or col>=9 : #if the row or column is invalid
+            print("Invalid location. Try again !")
+            continue
+        guess = input("Enter the number : ")
+        try:
+            number = int(guess)
+        except ValueError:
+            print("Invalid input. Enter a number !")
+            continue
+        if userPuzzle[row][col] == 0 :
+            if puzzle[row][col] == number :
+                userPuzzle[row][col] = number
+            else :
+                lives -= 1
+                print(f'Wrong number ! You have {lives} lives left.')
+        else :
+            print("Sorry, try again !")
+            continue
+    if lives == 0 :
+        print(":(:(:(:(:(:(:(:(:(:(")
+        print(":(:( GAME OVER  :(:(")
+        print(":(:(:(:(:(:(:(:(:(:(")
+        printPuzzle(puzzle)
+    else :
+        print("**********************************")
+        print("** CONGRATULATIONS, YOU WIN !!! **")
+        print("**********************************")
+
+def printPuzzle(puzzle) :
+    for r in range(9) :
+        row = [puzzle[r][c] for c in range(0,3)]
+        row1 = [puzzle[r][c] for c in range(3,6)]
+        row2 = [puzzle[r][c] for c in range(6,9)]
+        print(row, row1, row2)
+        if r % 3 == 2:
+            print('')
+
+
+if __name__=='__main__' :
+    print("***** Welcome, this is a sudoku game ! Let's play, goooooo *****\n")
+    puzzle = newPuzzle()
+    solveSudoku(puzzle)
+    userPuzzle = userPuzzle(puzzle)
+    play(puzzle, userPuzzle)
 
